b16e6fe92036597aeb33737bf8008c2c5c912cec
Stephen Lee <srlee@ucsd.edu>
2020-10-23 00:12:38 +0900

    upgrade features for room generator

--- room_assigner.py
+++ room_assigner.py
@@ -1,8 +1,10 @@
 # Created by: 龍ONE
 # Date Created: October 1, 2020
-# Date Edited: October 13, 2020
+# Date Edited: October 23, 2020
 # Purpose: Pseudo-randomly assign people to breakout rooms.
 
+# import copy for deepcopy list
+import copy
 # import sys for exiting file
 import sys
 
@@ -10,45 +12,71 @@ import sys
 def room_assigner(num_breakout_rooms, breakout_room, people_lst_m, people_lst_f, people_lst_all):
     # pseudo-randomly assign people to rooms
     ppl_in_room = []
-    room_num_newcomer_m = 0
-    # assign newcomer guys to rooms
+    # assign male leaders to rooms
+    room_num_leader_m = 0
     for person in people_lst_m:
-        if '(N)' in person:
-            breakout_room[room_num_newcomer_m].append(person)
+        if '(L)' in person:
+            breakout_room[room_num_leader_m].append(person)
             ppl_in_room.append(person)
-            room_num_newcomer_m += 1
-            if room_num_newcomer_m > num_breakout_rooms - 1:
-                room_num_newcomer_m = 0
+            room_num_leader_m += 1
+            if room_num_leader_m > num_breakout_rooms - 1:
+                room_num_leader_m = 0
     people_lst_m = [ppl for ppl in people_lst_m if ppl not in ppl_in_room]
-    room_num = room_num_newcomer_m
-    # asssign the rest of guys to rooms
+    # push all male newcomers to front of list
+    newcomers_m = False
+    people_lst_m_temp = []
+    for person in people_lst_m:
+        if ('(N)') in person:
+            people_lst_m_temp.append(person)
+            newcomers_m = True
+    for person in people_lst_m:
+        if ('(N)') not in person:
+            people_lst_m_temp.append(person)
+    if newcomers_m == True:
+        people_lst_m = people_lst_m_temp
+    # assign rest of males to rooms
+    room_num = room_num_leader_m
     for person in people_lst_m:
         breakout_room[room_num].append(person)
         ppl_in_room.append(person)
         room_num += 1
         if room_num > num_breakout_rooms - 1:
             room_num = 0
-    # assign newcomer girls to rooms
-    room_num_newcomer_f = room_num_newcomer_m
-    newcomer_f = False
+    people_lst_m = [ppl for ppl in people_lst_m if ppl not in ppl_in_room]
+    # assign female leaders to rooms
+    leaders_f = False
+    room_num_leader_f = room_num_leader_m
     for person in people_lst_f:
-        if '(N)' in person:
-            newcomer_f = True
-            breakout_room[room_num_newcomer_f].append(person)
+        if '(L)' in person:
+            breakout_room[room_num_leader_f].append(person)
             ppl_in_room.append(person)
-            room_num_newcomer_f += 1
-            if room_num_newcomer_f > num_breakout_rooms - 1:
-                room_num_newcomer_f = 0
+            room_num_leader_f += 1
+            if room_num_leader_f > num_breakout_rooms - 1:
+                room_num_leader_f = 0
+            leaders_f = True
     people_lst_f = [ppl for ppl in people_lst_f if ppl not in ppl_in_room]
-    if newcomer_f == True:
-        room_num = room_num_newcomer_f
-    # asssign the rest of girls to rooms
+    # push all female newcomers to front of list
+    newcomers_f = False
+    people_lst_f_temp = []
+    for person in people_lst_f:
+        if ('(N)') in person:
+            people_lst_f_temp.append(person)
+            newcomers_f = True
+    for person in people_lst_f:
+        if ('(N)') not in person:
+            people_lst_f_temp.append(person)
+    if newcomers_f == True:
+        people_lst_f = people_lst_f_temp
+    # assign rest of females to rooms
+    if leaders_f == True:
+        room_num = room_num_leader_f
     for person in people_lst_f:
         breakout_room[room_num].append(person)
         ppl_in_room.append(person)
         room_num += 1
         if room_num > num_breakout_rooms - 1:
             room_num = 0
+    people_lst_f = [ppl for ppl in people_lst_f if ppl not in ppl_in_room]
     # check if number of people in the room match total list of people
     if len(ppl_in_room) != len(people_lst_all):
         print('SOMETHING WENT WRONG!')
--- room_generator.py
+++ room_generator.py
@@ -1,8 +1,10 @@
 # Created by: 龍ONE
 # Date Created: October 1, 2020
-# Date Edited: October 6, 2020
+# Date Edited: October 23, 2020
 # Purpose: Generate breakout rooms for zoom call.
 
+# import counter for finding duplicate tuples
+from collections import Counter
 # import copy for deepcopy list
 import copy
 # import random for shuffling list
@@ -11,57 +13,73 @@ import random
 import room_assigner
 
 # create list for people present, and assign value for breakout rooms and event name
-num_new_ppl = 0
-people_lst_f = []
-people_lst_m = []
+start_list = False
+people_lst_all = []
 present = open('present.txt', 'r')
-start_list_f = False
-start_list_m = False
 for line in present:
     if 'Desired number of ppl per room:' in line:
         ppl_per_room = int(
             line[len('Desired number of ppl per room: '):].strip())
     elif 'Event Name:' in line:
         event_name = line[11:].strip()
-    elif 'FEMALES:' in line:
-        start_list_f = True
-        start_list_m = False
-    elif 'MALES:' in line:
-        start_list_m = True
-    if len(line.strip()) != 0:
-        if '(N)' in line.strip() and '**' not in line.strip():
-            num_new_ppl += 1
-        if start_list_m == True:
-            people_lst_m.append(line.strip())
-        if start_list_f == True:
-            people_lst_f.append(line.strip())
+    elif 'PRESENT:' in line:
+        start_list = True
+        continue
+    if len(line.strip()) != 0 and start_list == True:
+        people_lst_all.append(line.strip())
 
-# look for previous groups and add tuples if applicable
-groups_lst = []
-previous_rooms = open('previous-rooms.txt', 'r')
+# look for previous groups
 search_groups = False
-tuples_lst = []
+past_groups_lst = []
+previous_rooms = open('previous-rooms.txt', 'r')
 for line in previous_rooms:
     if 'EVENT: ' + event_name in line and '(Y)' in line:
         search_groups = True
         continue
     if search_groups == True:
-        if 'EVENT: ' in line:
-            break
         if len(line.strip()) != 0:
-            groups_lst.append(line.strip().split(','))
-for groups in groups_lst:
+            past_groups_lst.append(line.strip().split(','))
+
+# add tuples for past pairs
+past_pairs_lst = []
+for groups in past_groups_lst:
     for index_one in range(len(groups)):
         for index_two in range(index_one + 1, len(groups)):
-            tuples_lst.append(
-                (groups[index_one].strip(), groups[index_two].strip()))
+            current_pair = (groups[index_one].strip(),
+                            groups[index_two].strip())
+            past_pairs_lst.append(tuple(sorted(current_pair)))
 
-# create list of all people
-people_lst_all = []
-people_lst_m = people_lst_m[1:]
-people_lst_f = people_lst_f[1:]
-people_lst_all.extend(people_lst_m)
-people_lst_all.extend(people_lst_f)
+# sort past pairs to account for duplicate tuples
+past_pairs_dict = dict(Counter(pairs for pairs in past_pairs_lst))
+
+# generate master list with all people and their genders
+master_list = dict()
+master = open('master.txt', 'r')
+for line in master:
+    if '(M)' in line:
+        master_list[line.replace('(M)', '').strip()] = 'M'
+    elif '(F)' in line:
+        master_list[line.replace('(F)', '').strip()] = 'F'
+
+# separate list of people to male and female
+people_lst_m = []
+people_lst_f = []
+for name in people_lst_all:
+    name_str = name.replace('(N)', '').replace('(L)', '').strip()
+    if name_str in master_list:
+        if master_list[name_str] == 'M':
+            people_lst_m.append(name)
+        elif master_list[name_str] == 'F':
+            people_lst_f.append(name)
+    else:
+        user_input = ''
+        while user_input != 'M' and user_input != 'F':
+            user_input = input('Input the gender for: ' +
+                               name_str + '\nType M for male, F for female\n')
+            if user_input == 'M':
+                people_lst_m.append(name)
+            elif user_input == 'F':
+                people_lst_f.append(name)
 
 # randomize lists
 random.shuffle(people_lst_m)
@@ -74,42 +92,62 @@ for num in range(num_breakout_rooms):
     breakout_room.append([])
 
 # generate breakout rooms
-if len(tuples_lst) == 0:
+if len(past_pairs_lst) == 0:
     room_assigner.room_assigner(
         num_breakout_rooms, breakout_room, people_lst_m, people_lst_f, people_lst_all)
 else:
     lowest_error_val = -1000000
     temp_breakout_room = copy.deepcopy(breakout_room)
     best_breakout_room = []
+    lowest_error_val_lst = []
     for num in range(1000):
         error_val = 0
+        error_val_lst = []
         breakout_room = copy.deepcopy(temp_breakout_room)
         random.shuffle(people_lst_m)
         random.shuffle(people_lst_f)
         room_assigner.room_assigner(
             num_breakout_rooms, breakout_room, people_lst_m, people_lst_f, people_lst_all)
+        breakout_room = [[name.replace('(N)', '').replace(
+            '(L)', '').strip() for name in groups] for groups in breakout_room]
         for groups in breakout_room:
-            for tuple_pair in tuples_lst:
-                if tuple_pair[0] in groups and tuple_pair[1] in groups:
-                    error_val -= 1
+            for key in past_pairs_dict:
+                if key[0] in groups and key[1] in groups:
+                    if past_pairs_dict[key] == 1:
+                        error_val -= 1
+                        error_val_lst.append(1)
+                    else:
+                        error_val -= 5 * (past_pairs_dict[key] - 1)
+                        error_val_lst.append(5 * (past_pairs_dict[key] - 1))
         if error_val == 0:
             best_breakout_room = copy.deepcopy(breakout_room)
             break
         if error_val > lowest_error_val:
             lowest_error_val = error_val
             best_breakout_room = copy.deepcopy(breakout_room)
+            lowest_error_val_lst = copy.deepcopy(error_val_lst)
+
 
 # select the best breakout room if applicable
-if len(tuples_lst) != 0:
+if len(past_pairs_lst) != 0:
     breakout_room = best_breakout_room
 
 # print breakout rooms
 b_room_num = 1
 for groups in breakout_room:
-    print(('Breakout Room ' + str(b_room_num) + ': ' +
-           ', '.join(groups)).replace(' (N)', ''))
+    print('Breakout Room ' + str(b_room_num) + ': ' +
+          ', '.join(groups))
     b_room_num += 1
 
 # print error value if applicable
-if len(tuples_lst) != 0:
-    print('Error Val = ', error_val)
+if len(past_pairs_lst) != 0:
+    print('Error Val = ', lowest_error_val)
+
+# print error value list if applicable
+if error_val != 0:
+    error_val_dict = dict(Counter(values for values in lowest_error_val_lst))
+    for value in sorted(error_val_dict.keys()):
+        if value == 1:
+            print('Number of people that have shared 1 room together before:', error_val_dict[value])
+        else:
+            print('Number of people that have shared', int(value/5) + 1, 'rooms together before:', error_val_dict[value])
